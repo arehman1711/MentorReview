1e7ad6cbb7bf9a70a2f7a9424b25da745f31fbff
Abdur Rehman <a.rehman1711@gmail.com>
2023-09-18 21:02:43 +0530

    initial commit

---
+++ src/main/java/com/example/mentorreview/MentorReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mentorreview/MentorReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
